4c65e96b7ea78f2732a5335d0c581b42bc890c8b
Shavit Cohen <shavit.hit@gmail.com>
2016-06-20 15:53:13 +0300

    ide files

--- Gruntfile.js
+++ Gruntfile.js
@@ -240,7 +240,8 @@ module.exports = function (grunt) {
     filerev: {
       dist: {
         src: [
-          '<%= yeoman.dist %>/scripts/{,*/}*.js',
+          '<%= yeoman.dist %>/scripts/**/{,*/}*.js',
+          '<%= yeoman.dist %>/data/**/*',
           '<%= yeoman.dist %>/styles/{,*/}*.css',
           '<%= yeoman.dist %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}',
           '<%= yeoman.dist %>/styles/fonts/*'
